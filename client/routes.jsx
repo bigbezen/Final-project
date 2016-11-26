803ba1be000dec699cc5ec2c7280f92fc8e05714
shahafstein <shahafstein@gmail.com>
2016-11-26 14:08:23 +0200

    new project

---
+++ client/routes.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {mount} from 'react-mounter';
+
+import {MainLayout} from './layouts/MainLayouts.jsx';
+import App from '../App.jsx'
+
+FlowRouter.route('/', {
+	action() {
+		mount(MainLayout, {
+				content: (<App />)
+		})
+	}
+});
