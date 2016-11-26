c41709e2b0e4ec46529dc526f4ee05f5a310de2c
shahafstein <shahafstein@gmail.com>
2016-11-26 14:34:19 +0200

    rearranged App.jsx

--- client/routes.jsx
+++ client/routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {mount} from 'react-mounter';
 
 import {MainLayout} from './layouts/MainLayouts.jsx';
-import App from '../App.jsx'
+import App from './App.jsx'
 
 FlowRouter.route('/', {
 	action() {
